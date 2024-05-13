bbcc3e912c7fa8b9f704f8c1f2ba747eac3567b5
Tai Le <taiminhle4920@gmail.com>
2024-05-13 15:42:31 -0700

    converting the buy and sell day from it to date

--- app/app.py
+++ app/app.py
@@ -1,9 +1,9 @@
 
 from flask import Flask, request, jsonify, render_template
 import logging
-from utility import convert_date_to_n
+from utility import convert_date_to_n, trading_strategy
 from model import predict
-
+from datetime import timedelta
 app = Flask(__name__)
 app.debug = True
 
@@ -15,6 +15,19 @@ def index():
               num_of_days = convert_date_to_n(date)
               pred = predict(num_of_days)
               print(pred, flush=True)
+              
+              # buy_day, sell_day = trading_strategy(pred) 
+              # this will return the sale and buy date by adding the buy_day and sell_day to the choosen date
+              # if the buy_day or sell_day is -1 then it will return NA
+              buy_day, sell_day = trading_strategy(pred)
+              if buy_day == -1:
+                     date = "NA"
+              else:
+                     buy_date = date + timedelta(days=buy_day)
+              if sell_day == -1:
+                     sell_date = "NA"
+              else:
+                     sell_date = date + timedelta(days=sell_day)
               
               return pred
        return render_template('index.html')
